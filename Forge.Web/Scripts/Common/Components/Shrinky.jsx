367d46ed0e1aa973fc9da83d9c1cd280187b2bcc
Tatiana Veratti <Tatiana.Veratti@Cerner.com>
2017-07-31 17:48:57 -0500

    Shrinky

---
+++ Forge.Web/Scripts/Common/Components/Shrinky.jsx
@@ -0,0 +1,39 @@
+﻿const React = require('react');
+
+// =====================================
+const Shrinky = React.createClass({
+    // -----------------------------
+    render: function(){
+        const { shrink } = this.state;
+        const modifier = shrink ? 'shrink' : 'full';
+        const className = `shrinky skrinky--${modifier}`;
+
+        // When shrink is true, only the first child will remain visible.
+
+        return (
+            <div className={className}>
+                {this.props.children}
+            </div>
+        );
+    },
+
+    // -----------------------------
+    getInitialState: function(){
+        return { shrink: document.scrollTop > 100 };
+    },
+
+    // -----------------------------
+    componentDidMount: function(){
+        document.addEventListener('scroll', this.checkScroll);
+    },
+
+    // -----------------------------
+    checkScroll: function(ev){
+        const shrink = document.scrollTop > 100;
+        if (this.state.shrink !== shrink) {
+            this.setState({ shrink });
+        }
+    }
+});
+
+module.exports = Banner;
